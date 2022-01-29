abe744b7e8feddddeca084ca1ca8025825624fa1
awaisspk <iamawaisiqball@gmail.com>
2022-01-29 22:30:32 +0500

    feat(data): add categories data

---
+++ src/data/categories.ts
@@ -0,0 +1,52 @@
+type Categories = {
+  work: string;
+  img: string;
+  title: string;
+  count: number;
+  color: "blue" | "black";
+}[];
+
+export const categories: Categories = [
+  {
+    work: "designer",
+    img: "/images/designer.jpg",
+    title: "Graphic designer",
+    count: 300,
+    color: "blue",
+  },
+  {
+    work: "designer",
+    img: "/images/designer.jpg",
+    title: "User experience designer",
+    count: 300,
+    color: "black",
+  },
+  {
+    work: "designer",
+    img: "/images/designer.jpg",
+    title: "User interface designer",
+    count: 300,
+    color: "blue",
+  },
+  {
+    work: "designer",
+    img: "/images/designer.jpg",
+    title: "Animator",
+    count: 300,
+    color: "black",
+  },
+  {
+    work: "designer",
+    img: "/images/designer.jpg",
+    title: "Ux/UI expert",
+    count: 300,
+    color: "blue",
+  },
+  {
+    work: "designer",
+    img: "/images/designer.jpg",
+    title: "Animator",
+    count: 300,
+    color: "black",
+  },
+];
