a4d2c456591ce1eec1f235c77c26e86d0c25c000
awaisspk <iamawaisiqball@gmail.com>
2022-01-29 18:11:37 +0500

    feat(page): add hero section to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+import { HeroSection } from "@components/Hero";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  );
 };
 
 export default Home;
