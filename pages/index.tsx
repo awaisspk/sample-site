2148210d473aa2a31a721c462aed024030d8a103
awaisspk <iamawaisiqball@gmail.com>
2022-01-29 22:31:41 +0500

    styles(global): add carousel styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import { HeroSection } from "@components/Hero";
+import { Skills } from "@components/Skills";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <div>
       <HeroSection />
+      <Skills />
     </div>
   );
 };
