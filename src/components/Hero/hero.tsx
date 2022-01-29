d67dd1dbca14891a002f82ddd04fe8dd6d911679
awaisspk <iamawaisiqball@gmail.com>
2022-01-29 18:08:55 +0500

    feat(component): add hero section

---
+++ src/components/Hero/hero.tsx
@@ -0,0 +1,11 @@
+import { Banner1 } from "./Banner1/banner1";
+import { Banner2 } from "./Banner2/banner2";
+
+export const HeroSection = () => {
+  return (
+    <div>
+      <Banner1 />
+      <Banner2 />
+    </div>
+  );
+};
